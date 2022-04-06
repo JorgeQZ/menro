78a6f7e4c016bdd6cd58d1c09fa61b0d2c78e2ec
Byron-Guevara <byronguevara@akevia.mx>
2022-04-06 16:37:35 -0500

    + detashes

--- home.php
+++ home.php
@@ -15,48 +15,52 @@ $args = array(
 $loop = new WP_Query($args);
 ?>
 
-<div class="blog-container">
-    <div class="grid-container">
-        <div class="grid-posts">
-            <?php
-            if($loop->have_posts()):
-                while($loop->have_posts()):
-                    $loop->the_post();
-                    ?>
-            <a class="item" rel="nofollow noopener" href="<?php the_permalink(); ?>">
-                <img src="<?php echo get_template_directory_uri().'/img/object.jpg'; ?>" alt="">
-                <div class="title-box">
-                    <div>
-                        <div class="title"><?php the_title(); ?></div>
-                        <div class="desc">
-                            <?php the_excerpt(); ?>
+<div class="wrapper-sec">
+
+    <div class="blog-container">
+        <div class="grid-container">
+            <div class="grid-posts">
+                <?php
+                if($loop->have_posts()):
+                    while($loop->have_posts()):
+                        $loop->the_post();
+                        ?>
+                <a class="item" rel="nofollow noopener" href="<?php the_permalink(); ?>">
+                    <img src="<?php echo get_template_directory_uri().'/img/object.jpg'; ?>" alt="">
+                    <div class="title-box">
+                        <div>
+                            <div class="title"><?php the_title(); ?></div>
+                            <div class="desc">
+                                <?php the_excerpt(); ?>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </a>
-            <?php
-                endwhile;
-            endif;
-            wp_reset_query();
-            $GLOBALS['wp_query']->max_num_pages = $loop->max_num_pages;
-            $pages = $loop->max_num_pages;
-            ?>
-        </div>
+                </a>
+                <?php
+                    endwhile;
+                endif;
+                wp_reset_query();
+                $GLOBALS['wp_query']->max_num_pages = $loop->max_num_pages;
+                $pages = $loop->max_num_pages;
+                ?>
+            </div>
 
-        <div class="pagination-container">
-            <?php
-            for ($i=1; $i <= $pages; $i++){
-                if (1 != $pages &&( !($i >= $paged+$range+1 || $i <= $paged-$range-1) || $pages <= $showitems )){
-                    if($paged == $i):
-                        echo "<span class='item current'>".$i."</span>";
-                    else:
-                        echo "<a href='".get_pagenum_link($i)."' class='item'>".$i."</a>";
-                    endif;
+            <div class="pagination-container">
+                <?php
+                for ($i=1; $i <= $pages; $i++){
+                    if (1 != $pages &&( !($i >= $paged+$range+1 || $i <= $paged-$range-1) || $pages <= $showitems )){
+                        if($paged == $i):
+                            echo "<span class='item current'>".$i."</span>";
+                        else:
+                            echo "<a href='".get_pagenum_link($i)."' class='item'>".$i."</a>";
+                        endif;
+                    }
                 }
-            }
-            ?>
+                ?>
+            </div>
         </div>
     </div>
+
 </div>
 
 <?php get_footer(); ?>
